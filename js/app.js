09aa54113a6b1901b655ce75f1669c00268d4d1e
zjria <zakaria.com.bd@gmail.com>
2023-11-21 11:44:21 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+// Navbar section
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarNav = document.querySelector('.navbar-nav');
+
+navbarToggler.addEventListener('click', () => {
+    navbarNav.classList.toggle('active');
+});
+
+
+// banner slick
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        centerMode: true,
+        variableWidth: false,
+        adaptiveHeight: true,
+        arrows: true,
+        lazyLoad: 'ondemand',
+        autoplay: false,
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear',
+        prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-angle-left"></i></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-angle-right"></i></button>'
+
+    });
+
+    $('.slider-right-section').slick({
+        arrows: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        centerMode: true,
+        variableWidth: false,
+        adaptiveHeight: true,
+        arrows: true,
+        lazyLoad: 'ondemand',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        fade: true,
+        cssEase: 'linear',
+        // prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-angle-left"></i></button>',
+        // nextArrow: '<button class="slide-arrow next-arrow"><i class="fa-solid fa-angle-right"></i></button>'
+
+    });
+});
+
+
